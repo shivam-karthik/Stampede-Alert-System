59a04a16ce82246ddacbd9dd16a6c96d70862b15
shivam karthik <86833590+shivam-karthik@users.noreply.github.com>
2022-07-19 17:53:17 +0530

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+SSID = "Your SSID from Twilio website"
+AUTH_TOKEN = 'Auth Token from Twilio Website'
+FROM_NUMBER = 'Number generated on Twilio Website'
+TO_NUMBER = 'Number which will recieve alerts'
+API_KEY = 'API KEY generated on Bolt Cloud'
+DEVICE_ID = 'Your Device Id on Bolt Cloud'
+FRAME_SIZE = 10
+MUL_FACTOR = 6
